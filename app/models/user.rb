26aec26cf979fcf2abfdb096b7dd53a8f1b3ed14
tanakafumiaki <fumiakishodai@gmail.com>
2019-10-27 14:45:30 +0900

    fix_DB_recreate_02_add_VALID_EMAIL_REGEX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, allow_blank: true }
 end
